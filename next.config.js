ff89cb1287839a22e2204e3ea4363302e3b789f6
kurtdami <biglaity@gmail.com>
2025-01-16 04:12:15 -0800

    fix: update Next.js output directory for Vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  distDir: '.next',
+  distDir: 'dist',
   images: {
     domains: ['incidents.fire.ca.gov'],
   },
